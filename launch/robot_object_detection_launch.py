fb1b6f38fd822d8a500ab373659ddc78ec9c38b9
reid <gravesreid@gmail.com>
2024-02-13 15:16:35 -0500

    added object detection driver launch files

---
+++ launch/robot_object_detection_launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='bittle_ros2',
+            executable='object_detection_driver',
+            name='object_detection_driver',
+            output='screen'
+        ),
+        Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+            name='camera',
+            output='screen',
+            parameters=[
+                {'video_device': '/dev/video0'},
+                {'output_encoding': 'rgb8'},
+                {'pixel_format': 'YUYV'},
+                {'image_size': [640,480]},
+                {'io_method': 'mmap'}
+            ]
+        )
+    ])
---
+++ launch/server_object_detection_launch.py
@@ -0,0 +1,11 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='bittle_ros2',
+            executable='object_detection_subscriber',
+            output='screen'
+        )
+        ])
