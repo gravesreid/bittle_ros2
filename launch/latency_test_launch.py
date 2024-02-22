c4f24fd7bcdbbc3641b29da6d0f560b2469cc3ee
reid <gravesreid@gmail.com>
2024-02-22 13:37:44 -0500

    added testing node for image streaming latency on Pi

---
+++ launch/latency_test_launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='bittle_ros2',
+            executable='latency_test',
+        ),
+        Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+            name='camera',
+            output='screen',
+            parameters=[
+                {'video_device': '/dev/video0'},
+                {'output_encoding': 'rgb8'},
+                {'pixel_format': 'YUYV'},
+                {'image_size': [640,480]},
+                {'io_method': 'mmap'},
+                {'image_raw.jpeg_quality' : 25}
+            ]
+        )
+    ])
