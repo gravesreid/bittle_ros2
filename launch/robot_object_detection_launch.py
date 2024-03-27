473824ce0848c0cd242b530f87fc3f9a8206dadf
Reid Graves <gravesreid@gmail.com>
2024-03-27 13:03:24 -0400

    updated robot_object_detection_launch

--- launch/robot_object_detection_launch.py
+++ launch/robot_object_detection_launch.py
@@ -5,8 +5,14 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='bittle_ros2',
-            executable='object_detection_driver',
-            name='object_detection_driver',
+            executable='object_detection_to_command',
+            name='object_detection_to_command',
+            output='screen'
+        ),
+        Node(
+            package='bittle_ros2',
+            executable='serial_sender',
+            name='serial_sender',
             output='screen'
         ),
         Node(
